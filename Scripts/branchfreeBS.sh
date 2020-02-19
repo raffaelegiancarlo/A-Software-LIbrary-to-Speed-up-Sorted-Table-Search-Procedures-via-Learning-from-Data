b51cf6958bc6930e1915a0bc5a1b0561c8fe71b6
DomenicoAmato01 <domenico.amato01@unipa.it>
2020-02-19 11:03:12 +0100

    Added BS bash

--- Scripts/branchfreeBS.sh
+++ Scripts/branchfreeBS.sh
@@ -1,2 +1,17 @@
 g++ ../Binary_Search_Software/branchfreeBS.cpp -o ../Binary_Search_Software/branchfreeBS
 
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 80 -n 100
