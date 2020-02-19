9e397ccfceab5dd27ae3bf5e1a9e12bf404d76cb
DomenicoAmato01 <domenico.amato01@unipa.it>
2020-02-19 13:46:30 +0100

    Added BS and IS scripts

--- Scripts/branchfreeBS.sh
+++ Scripts/branchfreeBS.sh
@@ -1,4 +1,5 @@
 g++ ../Binary_Search_Software/branchfreeBS.cpp -o ../Binary_Search_Software/branchfreeBS
+g++ ../Binary_Search_Software/eytzBS.cpp -o ../Binary_Search_Software/eytzBS
 
 ../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 10 -n 100
 ../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 50 -n 100
@@ -8,10 +9,26 @@ g++ ../Binary_Search_Software/branchfreeBS.cpp -o ../Binary_Search_Software/bran
 ../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 50 -n 100
 ../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3.sorted -o branchfreeBS.csv -q 80 -n 100
+
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4.sorted -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4.sorted -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4.sorted -o branchfreeBS.csv -q 80 -n 100
---
+++ Scripts/interpolationSearch.sh
@@ -0,0 +1,33 @@
+g++ ../Interpolation_Search/interpolationSearch.cpp -o ../Interpolation_Search/interpolationSearch
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03.sorted -o interpolationSearch.csv -q 80 -n 100
+
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04.sorted -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04.sorted -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04.sorted -o interpolationSearch.csv -q 80 -n 100
