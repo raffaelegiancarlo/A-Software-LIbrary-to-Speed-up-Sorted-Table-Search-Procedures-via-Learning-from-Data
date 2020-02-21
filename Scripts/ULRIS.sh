bd021d03d7fb73666ba8781aba12e5c706cc95ea
DomenicoAmato01 <49783406+DomenicoAmato01@users.noreply.github.com>
2020-02-21 18:35:47 +0100

    Add files via upload

--- Scripts/ULRIS.sh
+++ Scripts/ULRIS.sh
@@ -1,41 +1,41 @@
 g++ ../Learned_sorted_table_search/ULRIS.cpp -o ../Learned_sorted_table_search/ULRIS
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni01.sorted -o ULRIS.csv -q 10 -n 100 -e 9
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni01.sorted -o ULRIS.csv -q 50 -n 100 -e 9
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni01.sorted -o ULRIS.csv -q 80 -n 100 -e 9
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni01 -o ULRIS.csv -q 10 -n 100 -e 9
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni01 -o ULRIS.csv -q 50 -n 100 -e 9
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni01 -o ULRIS.csv -q 80 -n 100 -e 9
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni02.sorted -o ULRIS.csv -q 10 -n 100 -e 56
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni02.sorted -o ULRIS.csv -q 50 -n 100 -e 56
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni02.sorted -o ULRIS.csv -q 80 -n 100 -e 56
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni02 -o ULRIS.csv -q 10 -n 100 -e 56
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni02 -o ULRIS.csv -q 50 -n 100 -e 56
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni02 -o ULRIS.csv -q 80 -n 100 -e 56
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni03.sorted -o ULRIS.csv -q 10 -n 100 -e 620
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni03.sorted -o ULRIS.csv -q 50 -n 100 -e 620
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni03.sorted -o ULRIS.csv -q 80 -n 100 -e 620
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni03 -o ULRIS.csv -q 10 -n 100 -e 620
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni03 -o ULRIS.csv -q 50 -n 100 -e 620
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni03 -o ULRIS.csv -q 80 -n 100 -e 620
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni04.sorted -o ULRIS.csv -q 10 -n 100 -e 7493
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni04.sorted -o ULRIS.csv -q 50 -n 100 -e 7493
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni04.sorted -o ULRIS.csv -q 80 -n 100 -e 7493
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni04 -o ULRIS.csv -q 10 -n 100 -e 7493
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni04 -o ULRIS.csv -q 50 -n 100 -e 7493
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d uni04 -o ULRIS.csv -q 80 -n 100 -e 7493
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn01.sorted -o ULRIS.csv -q 10 -n 100 -e 124
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn01.sorted -o ULRIS.csv -q 50 -n 100 -e 124
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn01.sorted -o ULRIS.csv -q 80 -n 100 -e 124
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn01 -o ULRIS.csv -q 10 -n 100 -e 124
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn01 -o ULRIS.csv -q 50 -n 100 -e 124
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn01 -o ULRIS.csv -q 80 -n 100 -e 124
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn02.sorted -o ULRIS.csv -q 10 -n 100 -e 1921
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn02.sorted -o ULRIS.csv -q 50 -n 100 -e 1921
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn02.sorted -o ULRIS.csv -q 80 -n 100 -e 1921
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn02 -o ULRIS.csv -q 10 -n 100 -e 1921
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn02 -o ULRIS.csv -q 50 -n 100 -e 1921
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn02 -o ULRIS.csv -q 80 -n 100 -e 1921
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn03.sorted -o ULRIS.csv -q 10 -n 100 -e 239668
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn03.sorted -o ULRIS.csv -q 50 -n 100 -e 239668
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn03.sorted -o ULRIS.csv -q 80 -n 100 -e 239668
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn03 -o ULRIS.csv -q 10 -n 100 -e 239668
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn03 -o ULRIS.csv -q 50 -n 100 -e 239668
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn03 -o ULRIS.csv -q 80 -n 100 -e 239668
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn04.sorted -o ULRIS.csv -q 10 -n 100 -e 55976156
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn04.sorted -o ULRIS.csv -q 50 -n 100 -e 55976156
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn04.sorted -o ULRIS.csv -q 80 -n 100 -e 55976156
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn04 -o ULRIS.csv -q 10 -n 100 -e 55976156
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn04 -o ULRIS.csv -q 50 -n 100 -e 55976156
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d logn04 -o ULRIS.csv -q 80 -n 100 -e 55976156
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-wl.sorted -o ULRIS.csv -q 10 -n 100 -e 1794
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-wl.sorted -o ULRIS.csv -q 50 -n 100 -e 1794
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-wl.sorted -o ULRIS.csv -q 80 -n 100 -e 1794
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-wl -o ULRIS.csv -q 10 -n 100 -e 1794
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-wl -o ULRIS.csv -q 50 -n 100 -e 1794
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-wl -o ULRIS.csv -q 80 -n 100 -e 1794
 
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-iot.sorted -o ULRIS.csv -q 10 -n 100 -e 534810
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-iot.sorted -o ULRIS.csv -q 50 -n 100 -e 534810
-../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-iot.sorted -o ULRIS.csv -q 80 -n 100 -e 534810
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-iot -o ULRIS.csv -q 10 -n 100 -e 534810
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-iot -o ULRIS.csv -q 50 -n 100 -e 534810
+../Learned_sorted_table_search/ULRIS -p ../Resource/ -d real-iot -o ULRIS.csv -q 80 -n 100 -e 534810
--- Scripts/branchfreeBS.sh
+++ Scripts/branchfreeBS.sh
@@ -1,42 +1,42 @@
 g++ ../Binary_Search_Software/branchfreeBS.cpp -o ../Binary_Search_Software/branchfreeBS
 g++ ../Binary_Search_Software/eytzBS.cpp -o ../Binary_Search_Software/eytzBS
 
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni01 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d uni02 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni03 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d uni04 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn01 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/branchfreeBS -p ../Resource/ -d logn02 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn3 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4 -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4 -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d logn4 -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/eytzBS -p ../Resource/ -d real-iot.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d real-iot.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d real-iot.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d real-iot -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d real-iot -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d real-iot -o branchfreeBS.csv -q 80 -n 100
 
-../Binary_Search_Software/eytzBS -p ../Resource/ -d real-wl.sorted -o branchfreeBS.csv -q 10 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d real-wl.sorted -o branchfreeBS.csv -q 50 -n 100
-../Binary_Search_Software/eytzBS -p ../Resource/ -d real-wl.sorted -o branchfreeBS.csv -q 80 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d real-wl -o branchfreeBS.csv -q 10 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d real-wl -o branchfreeBS.csv -q 50 -n 100
+../Binary_Search_Software/eytzBS -p ../Resource/ -d real-wl -o branchfreeBS.csv -q 80 -n 100
