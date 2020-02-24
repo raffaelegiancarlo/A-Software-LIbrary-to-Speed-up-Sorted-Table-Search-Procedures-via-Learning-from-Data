6ce7cca99befad2567febfd0c68fa79ca94c6149
DomenicoAmato01 <domenico.amato01@unipa.it>
2020-02-24 09:35:48 +0100

    Fixed interpolation search

--- Scripts/interpolationSearch.sh
+++ Scripts/interpolationSearch.sh
@@ -1,41 +1,41 @@
 g++ ../Interpolation_Search/interpolationSearch.cpp -o ../Interpolation_Search/interpolationSearch
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni01 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni02 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni03 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d uni04 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn01 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn02 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn03 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04 -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04 -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d logn04 -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-wl. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-wl. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-wl. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-wl -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-wl -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-wl -o interpolationSearch.csv -q 80 -n 100
 
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-iot. -o interpolationSearch.csv -q 10 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-iot. -o interpolationSearch.csv -q 50 -n 100
-../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-iot. -o interpolationSearch.csv -q 80 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-iot -o interpolationSearch.csv -q 10 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-iot -o interpolationSearch.csv -q 50 -n 100
+../Interpolation_Search/interpolationSearch -p ../Resource/ -d real-iot -o interpolationSearch.csv -q 80 -n 100
